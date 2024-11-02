02feb7f1b9eb1ecc8b9d87bf18586b4c8b6e6a09
rf777rf777 <rf777rf777@gmail.com>
2024-11-02 23:17:52 +0800

    add onnx test

--- source/onnx.py
+++ source/onnx.py
@@ -1,4 +1,3 @@
-# 安裝 ONNX Runtime
 import onnxruntime as ort
 import numpy as np
 import tensorflow as tf
@@ -6,13 +5,13 @@ import pandas as pd
 
 def process_image(image_path, img_size=224):
     """
-    將圖片路徑轉換為數值化的張量。
+    圖片路徑轉換為數值化張量。
     """
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.convert_image_dtype(image, tf.float32)
     image = tf.image.resize(image, size=[img_size, img_size])
-    image = np.expand_dims(image, axis=0)  # 增加 batch 維度
+    image = np.expand_dims(image, axis=0)  #增加batch維度
     return image
 
 def get_pred_label(prediction_probabilities):
@@ -21,17 +20,13 @@ def get_pred_label(prediction_probabilities):
     unique_breeds = np.unique(labels)
     return unique_breeds[np.argmax(prediction_probabilities)]
 
-# 加載 ONNX 模型
 onnx_model_path = 'core/model/dogVision.onnx'
 ort_session = ort.InferenceSession(onnx_model_path)
 
-# 載入並處理圖片
 image = process_image('dog.jpg')
 
-# 進行推理
 input_name = ort_session.get_inputs()[0].name
 predictions = ort_session.run(None, {input_name: image})
 
-# 取得預測結果
 predicted_label = get_pred_label(predictions[0])
 print(predicted_label)
